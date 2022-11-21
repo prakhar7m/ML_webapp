443ccf5834c180acddd6eef7d85819a0752b039a
prakhar7m <61321245+prakhar7m@users.noreply.github.com>
2022-11-21 17:33:43 -0500

    Update ml-app.py

--- ml-app.py
+++ ml-app.py
@@ -11,23 +11,6 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
-import warnings
-import regex as re
-warnings.filterwarnings('ignore')#to filter all the warnings
-import seaborn as sns
-pd.set_option('float_format', '{:.4f}'.format)# to keep the float values short
-# Import for wordcloud
-from os import path
-from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-
-#import fot plotly
-import plotly.express as px
 
 
 # In[38]:
